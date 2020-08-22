c7e6c2881bd19d6586128a49d8d5e2cf9b2da250
mrkolt01 <kolt.zar@mail.ru>
2020-08-22 13:55:08 +0300

    Updates

---
+++ precache-manifest.3ab971b6dba98accf4efaa318bfe6f0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0a024586d562e5e34cc260257fd3bd4",
+    "url": "/danko-hb/index.html"
+  },
+  {
+    "revision": "9b3fa4fce4b4bed625d8",
+    "url": "/danko-hb/static/css/main.3f2d1568.chunk.css"
+  },
+  {
+    "revision": "265894e49afe0985f27b",
+    "url": "/danko-hb/static/js/2.3056ae8e.chunk.js"
+  },
+  {
+    "revision": "e5a8fffa72502aa73a9d8af70f173712",
+    "url": "/danko-hb/static/js/2.3056ae8e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9b3fa4fce4b4bed625d8",
+    "url": "/danko-hb/static/js/main.6c5cc60f.chunk.js"
+  },
+  {
+    "revision": "42517a73660813ffda1c",
+    "url": "/danko-hb/static/js/runtime-main.d814e9c6.js"
+  },
+  {
+    "revision": "1d53cc674d79c883c5cc6a27821a6880",
+    "url": "/danko-hb/static/media/danko.1d53cc67.png"
+  }
+]);
